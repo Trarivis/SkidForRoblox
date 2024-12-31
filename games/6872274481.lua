906ec214d494330a9e5a84f67b5a71aaf5df442f
sstvskids <staavstudios@gmail.com>
2024-12-31 23:44:21 +1100

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1449,7 +1449,7 @@ run(function()
 							doAttack = doAttack or bedwars.SwordController:getTargetInRegion(attackRange or 3.8 * 3, 0)
 							if doAttack and Mode.Value == 'Player' then
 								for _, ent in entitylib.List do
-									bedwars.SwordController:swingSwordAtMouse(CFrame.new(ent.RootPart.Position - (Mouse.Origin - ent.RootPart.Position).Unit))
+									bedwars.SwordController:swingSwordAtMouse(CFrame.new(ent.RootPart.Position + (ent.RootPart.Position - Mouse.Origin).Unit))
 
 								end
 							elseif doAttack and Mode.Value == 'Mouse' then

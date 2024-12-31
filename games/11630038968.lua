c56dcab78009cbc6a655cc5d87c6d73d22cf35ef
sstvskids <staavstudios@gmail.com>
2025-01-01 01:34:52 +1100

    Update 11630038968.lua

--- games/11630038968.lua
+++ games/11630038968.lua
@@ -55,7 +55,7 @@ run(function()
 		Knit = Knit,
 		Entity = require(replicatedStorage.Modules.Entity),
 		ServerData = require(replicatedStorage.Modules.ServerData),
-		ViewmodelController = require(replicatedStorage.Modules.ViewmodelController),
+		ViewmodelController = Knit.GetController('ViewmodelController'),
 	}, {
 		__index = function(self, ind)
 			rawset(self, ind, ind:find('Service') and Knit.GetService(ind) or Knit.GetController(ind))

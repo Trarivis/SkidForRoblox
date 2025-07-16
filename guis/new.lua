85f180763f9502b58c8f187a1cecec7754a222c1
Trarivis <trariviste@gmail.com>
2025-07-16 08:33:09 +0200

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -313,7 +313,7 @@ local function downloadFile(path, func)
 	if not isfile(path) then
 		createDownloader(path)
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/skidvape/SkidForRoblox/'..readfile('skid/profiles/commit.txt')..'/'..select(1, path:gsub('skid/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/Trarivis/SkidForRoblox/'..readfile('skid/profiles/commit.txt')..'/'..select(1, path:gsub('skid/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
